c4d64a8cfd72229b601f951fe43cac7204866a88
stephenkwan1668 <stephenkwan1668@hotmail.com>
2024-08-12 23:27:05 +0200

    Initial commit and push for setting up the project

---
+++ Vicronix/src/main/java/nl/vicronix/VicronixApplication.java
@@ -0,0 +1,13 @@
+package nl.vicronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VicronixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VicronixApplication.class, args);
+    }
+
+}
---
+++ Vicronix/src/test/java/nl/vicronix/VicronixApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.vicronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VicronixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
